e8e904ffbe417162711fa1cd4b94b90fd68bff82
vigneshram0706 <vigneshram0706@gmail.com>
2025-09-08 16:50:53 +0000

    Introducing actions and state changing reducers

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,10 +1,12 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer,on } from "@ngrx/store";
+import { increment } from "./counter.actions";
 const initialState = 0;
 
 // New approach
 export const counterReducer = createReducer(
-    initialState
-) //action, old state
+    initialState, 
+    on(increment,(state)=> state+1 )
+) 
 
 // Old version approach
 // export function createReducer(state=initialState){ //pass initial state as default value
