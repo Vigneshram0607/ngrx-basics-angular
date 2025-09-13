4b004a0a2c63b27cc4b14d7f58034af067142184
vigneshram0706 <vigneshram0706@gmail.com>
2025-09-13 10:27:59 +0000

    added selectors

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subscription } from 'rxjs';
 
 import { CounterService } from '../counter.service';
 import { Store } from '@ngrx/store';
+import { selectCount } from '../store/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -13,7 +14,8 @@ export class CounterOutputComponent  {
   count$: Observable<number>;
 
   constructor(private store: Store<{counter:number}>) {
-    this.count$ = store.select('counter');
+    // this.count$ = store.select('counter');
+    this.count$ = store.select(selectCount);
   }
 
 }
